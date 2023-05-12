27282536a757fbe0bcaab97cf944f4a0b8b36d83
Matthew Morgan <matthew@hidlen.com>
2023-05-12 11:04:51 +0100

    Intermediate commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 
 import 'dart:developer' as developer;
 
-
 void main() {
   return runApp(
     const MaterialApp(home: HomePage()),
@@ -27,14 +26,21 @@ class _HomePageState extends State<HomePage> {
   bool _foundDeviceWaitingToConnect = false;
   bool _scanStarted = false;
   bool _connected = false;
+  String _logData = "";
+
 // Bluetooth related variables
   late DiscoveredDevice _ubiqueDevice;
   final flutterReactiveBle = FlutterReactiveBle();
   late StreamSubscription<DiscoveredDevice> _scanStream;
-  late QualifiedCharacteristic _rxCharacteristic;
+
+  //late QualifiedCharacteristic _rxCharacteristic;
+  late QualifiedCharacteristic _logDataCharacteristic;
+
 // These are the UUIDs of your device
   final Uuid serviceUuid = Uuid.parse("e280122a-c45b-44dc-a340-d3ac899dc88b");
-  final Uuid characteristicUuid = Uuid.parse("40614d40-dab6-49b8-921e-a72261b844ba");
+
+  //final Uuid characteristicUuid = Uuid.parse("40614d40-dab6-49b8-921e-a72261b844ba");
+  final Uuid logDataCharacteristicUuid = Uuid.parse("5a8e5d70-7e5e-4a1f-8a2d-5a5e8c5f5ca5"); // Use the UUID of your logDataCharacteristic
 
   void _startScan() async {
 // Platform permissions handling stuff
@@ -52,8 +58,7 @@ class _HomePageState extends State<HomePage> {
     }
 // Main scanning logic happens here ⤵️
     if (permGranted) {
-      _scanStream = flutterReactiveBle
-          .scanForDevices(withServices: [serviceUuid]).listen((device) {
+      _scanStream = flutterReactiveBle.scanForDevices(withServices: [serviceUuid]).listen((device) {
         // TODO: Change this string to what you defined in arduino
         if (device.name == 'UBIQUE') {
           developer.log('found ubique', name: 'my.app.category');
@@ -71,112 +76,106 @@ class _HomePageState extends State<HomePage> {
     _scanStream.cancel();
     // Let's listen to our connection so we can make updates on a state change
     Stream<ConnectionStateUpdate> _currentConnectionStream = flutterReactiveBle
-        .connectToAdvertisingDevice(
-        id: _ubiqueDevice.id,
-        prescanDuration: const Duration(seconds: 1),
-        withServices: [serviceUuid, characteristicUuid]);
+        .connectToAdvertisingDevice(id: _ubiqueDevice.id, prescanDuration: const Duration(seconds: 1), withServices: [serviceUuid, logDataCharacteristicUuid]);
     _currentConnectionStream.listen((event) {
       switch (event.connectionState) {
-      // We're connected and good to go!
         case DeviceConnectionState.connected:
           {
-            _rxCharacteristic = QualifiedCharacteristic(
-                serviceId: serviceUuid,
-                characteristicId: characteristicUuid,
-                deviceId: event.deviceId);
+            _logDataCharacteristic = QualifiedCharacteristic(serviceId: serviceUuid, characteristicId: logDataCharacteristicUuid, deviceId: event.deviceId);
             setState(() {
               _foundDeviceWaitingToConnect = false;
               _connected = true;
             });
+            _subscribeToLogData(); // Add this line
             break;
           }
-      // Can add various state state updates on disconnect
-        case DeviceConnectionState.disconnected:
-          {
-            break;
-          }
-        default:
+        // ... (same as before)
       }
     });
   }
 
-  void _partyTime() {
-    if (_connected) {
-      flutterReactiveBle
-          .writeCharacteristicWithResponse(_rxCharacteristic, value: [
-        0xff,
-      ]);
-    }
+  void _subscribeToLogData() {
+    developer.log('subscribing to log data', name: 'my.app.category');
+    // Subscribe to notifications from logDataCharacteristic
+    flutterReactiveBle.subscribeToCharacteristic(_logDataCharacteristic).listen((data) {
+      // Convert received data to string
+      String receivedData = String.fromCharCodes(data);
+      developer.log('received data: ' + receivedData, name: 'my.app.category');
+      setState(() {
+        // Append received data to _logData
+        _logData += receivedData;
+        developer.log('received data: ' + receivedData, name: 'my.app.category');
+      });
+    });
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(),
+      //body: Container(),
+      body: Center(child: Text(_logData)),
       persistentFooterButtons: [
         // We want to enable this button if the scan has NOT started
         // If the scan HAS started, it should be disabled.
         _scanStarted
-        // True condition
+            // True condition
             ? ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.grey, // background
-            onPrimary: Colors.white, // foreground
-          ),
-          onPressed: () {},
-          child: const Icon(Icons.search),
-        )
-        // False condition
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey, // background
+                  onPrimary: Colors.white, // foreground
+                ),
+                onPressed: () {},
+                child: const Icon(Icons.search),
+              )
+            // False condition
             : ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.blue, // background
-            onPrimary: Colors.white, // foreground
-          ),
-          onPressed: _startScan,
-          child: const Icon(Icons.search),
-        ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue, // background
+                  onPrimary: Colors.white, // foreground
+                ),
+                onPressed: _startScan,
+                child: const Icon(Icons.search),
+              ),
         _foundDeviceWaitingToConnect
-        // True condition
+            // True condition
             ? ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.blue, // background
-            onPrimary: Colors.white, // foreground
-          ),
-          onPressed: _connectToDevice,
-          child: const Icon(Icons.bluetooth),
-        )
-        // False condition
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue, // background
+                  onPrimary: Colors.white, // foreground
+                ),
+                onPressed: _connectToDevice,
+                child: const Icon(Icons.bluetooth),
+              )
+            // False condition
             : ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.grey, // background
-            onPrimary: Colors.white, // foreground
-          ),
-          onPressed: () {},
-          child: const Icon(Icons.bluetooth),
-        ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey, // background
+                  onPrimary: Colors.white, // foreground
+                ),
+                onPressed: () {},
+                child: const Icon(Icons.bluetooth),
+              ),
         _connected
-        // True condition
+            // True condition
             ? ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.blue, // background
-            onPrimary: Colors.white, // foreground
-          ),
-          onPressed: _partyTime,
-          child: const Icon(Icons.celebration_rounded),
-        )
-        // False condition
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue, // background
+                  onPrimary: Colors.white, // foreground
+                ),
+                onPressed: _subscribeToLogData,
+                child: const Icon(Icons.celebration_rounded),
+              )
+            // False condition
             : ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.grey, // background
-            onPrimary: Colors.white, // foreground
-          ),
-          onPressed: () {},
-          child: const Icon(Icons.celebration_rounded),
-        ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey, // background
+                  onPrimary: Colors.white, // foreground
+                ),
+                onPressed: () {},
+                child: const Icon(Icons.celebration_rounded),
+              ),
       ],
-
     );
   }
 }
